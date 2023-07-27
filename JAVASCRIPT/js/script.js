41568425da81ee2b9161d41cce750775e859af1f
Czarek Lewarek <czarek.warek@wp.pl>
2023-07-27 16:11:00 +0200

    pierwszy commit

---
+++ JAVASCRIPT/js/script.js
@@ -0,0 +1,16 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var Playermove;
+PlayerMove = 'papier'
+printMessage('Zagrałem' + Playermove + '!Jeśli Twój ruch to nożyce, to wygrywasz!!')
