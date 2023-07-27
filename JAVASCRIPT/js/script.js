eed06614a235d5b8b027adb4e1861c94460b22f8
Czarek Lewarek <czarek.warek@wp.pl>
2023-07-27 16:32:57 +0200

    Edit Script

--- JAVASCRIPT/js/script.js
+++ JAVASCRIPT/js/script.js
@@ -1,16 +1,14 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-var computerMove;
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
 computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-var Playermove;
-PlayerMove = 'papier'
-printMessage('Zagrałem' + Playermove + '!Jeśli Twój ruch to nożyce, to wygrywasz!!')
+if (randomNumber == '2') {
+computerMove = 'papier';
+if (randomNumber == '3') {
+computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
