7df595fbcba3827c3e93e1753f2470fa52e96155
Czarek Lewarek <czarek.warek@wp.pl>
2023-08-01 19:23:31 +0200

    Should works

--- JAVASCRIPT/js/functions.js
+++ JAVASCRIPT/js/functions.js
@@ -8,8 +8,6 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-// get move name//
-
 function getMoveName(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
@@ -23,5 +21,45 @@ function getMoveName(argMoveId){
     printMessage('Nie znam ruchu o id ' + argMoveId + '.');
     return 'nieznany ruch';
   }
+  function displayResult(argPlayerMove, argComputerMove) {
+    console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+    if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+      printMessage('Wygrywasz!');
+    }
+    else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+      printMessage('Wygrywasz!');
+      }
+    else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+        printMessage('Wygrywasz!');
+      }
+    else if (argPlayerMove == argComputerMove) {
+      printMessage('Remis!')
+    }  
+    else {
+      printMessage('Przegrywasz :(');
+    }
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  }
+  
+  playerMove = argButtonName;
 
-  // funkcja display //
+  console.log('ruch gracza to: ' + playerMove);
+  randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('wylosowana liczba to: ' + randomNumber);
+  computerMove = getMoveName(randomNumber);
+  console.log('ruch komputera to: ' + computerMove);
+  displayResult(playerMove, computerMove);
+
+  function buttonClicked(argButtonName) {
+    clearMessages();
+    console.log(argButtonName + ' został kliknięty');}
+
+
+    function argButtonName {
+      button-rock.addEventListener('click', function(){ 
+        buttonClicked('Guzik kamień'); });
+    button-paper.addEventListener('click', function(){ 
+        buttonClicked('Guzik papier'); });
+    button-scissors.addEventListener('click', function(){ 
+        buttonClicked('Guzik nożyce'); });
+    }
